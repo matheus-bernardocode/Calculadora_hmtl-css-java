8c9498217cc7e8228008ce9c2d9f382446456983
matheus-bernardocode <matheus_ruekl@estudante.sesisenai.org>
2026-03-18 09:10:53 -0300

    ADD - Finalizei  o código, com o acerto das funcoes no java.

--- funcao.js
+++ funcao.js
@@ -8,4 +8,24 @@ function clean()
     document.getElementById('resultado').innerHTML = '';
 }
 
+function apagar()
+{
+    var resultado = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1 )
+    
+}
  
+function calcular()
+{
+    var resultado = document.getElementById('resultado').innerHTML
+    if(resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
+    else
+    {
+        document.getElementById('resultado').innerHTML = "Nada..."
+    }
+
+}   
+    
